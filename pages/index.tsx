f37a7410ca8218ac19e37a9a4420015356e76d19
mainHome <a11dc094@dhw.ac.jp>
2022-05-01 22:10:15 +0900

    一覧，投稿部分

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,26 +29,43 @@ const Home: NextPage = () => {
     <>
       {session ? (
         <div className={styles.main}>
-          <button onClick={() => signOut()}>サインアウト</button>
+          <div className={styles.contentmain}>
+            <div>
+              <button onClick={() => signOut()}>サインアウト</button>
+            </div>
 
-          <div>
-            <p>投稿メッセージ</p>
-            {messages.map((item: any) => {
-              return <div key={item.id}>*{item.message}*</div>;
-            })}
+            <div className={styles.contentmessages}>
+              <p>投稿メッセージ</p>
+              {messages.map((item: any) => {
+                return (
+                  <div className={styles.contentmessageitem} key={item.id}>
+                    {item.message}
+                  </div>
+                );
+              })}
+              <button onClick={() => refreshMessage()}>更新</button>
+            </div>
+            <div style={{ display: "flex", width: "100%" }}>
+              <textarea
+                name="value"
+                id="value"
+                style={{ width: "100%" }}
+                cols={80}
+                rows={3}
+                value={newMessage.value}
+                onChange={(e) => setMessage(e)}
+              ></textarea>
+            </div>
+            <div style={{ display: "flex", width: "100%" }}>
+              <button
+                id={"submit"}
+                style={{ width: "100%" }}
+                onClick={() => handlePost()}
+              >
+                Submit
+              </button>
+            </div>
           </div>
-          <button onClick={() => refreshMessage()}>更新</button>
-          <textarea
-            name="value"
-            id="value"
-            cols={80}
-            rows={3}
-            value={newMessage.value}
-            onChange={(e) => setMessage(e)}
-          ></textarea>
-          <button id={"submit"} onClick={() => handlePost()}>
-            Submit
-          </button>
         </div>
       ) : (
         <div className={styles.main}>
@@ -56,7 +73,7 @@ const Home: NextPage = () => {
             <div className={styles.title}>
               <h1>LANSHAREWARE</h1>
             </div>
-            <div style={{width:"100%",display:"flex"}}>
+            <div style={{ width: "100%", display: "flex" }}>
               <div className={styles.loginitem}>
                 <label>
                   メールアドレス：
