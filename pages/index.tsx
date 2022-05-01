2fd43cdf4028eea90fb511177526f75a224e7731
mainHome <a11dc094@dhw.ac.jp>
2022-05-01 20:32:13 +0900

    login デザイン

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,34 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import User from "../hooks/useUser";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const { session, messages, signOut, signInWithPassword, refreshMessage, postMessage } =
-    User();
+  const {
+    session,
+    messages,
+    signOut,
+    signInWithPassword,
+    refreshMessage,
+    postMessage,
+  } = User();
   const [text, setText] = useState("");
-  const [newMessage,setMessage]=useState({value:''});
+  const [newMessage, setMessage] = useState({ value: "" });
 
   const handleChange = (e: any) => {
     setText(() => e.target.value);
   };
 
-  const handlePost=()=>{
-    console.log(newMessage)
-    postMessage(newMessage.target.value)
+  const handlePost = () => {
+    console.log(newMessage);
+    postMessage(newMessage.target.value);
     refreshMessage();
-  }
+  };
 
   return (
     <>
       {session ? (
-        <div>
+        <div className={styles.main}>
           <button onClick={() => signOut()}>サインアウト</button>
 
           <div>
@@ -31,16 +38,43 @@ const Home: NextPage = () => {
             })}
           </div>
           <button onClick={() => refreshMessage()}>更新</button>
-          <textarea name="value" id="value" cols={80} rows={3} value={newMessage.value} onChange={(e)=>setMessage(e)}></textarea>
-          <button id={"submit"} onClick={() => handlePost()}>Submit</button>
+          <textarea
+            name="value"
+            id="value"
+            cols={80}
+            rows={3}
+            value={newMessage.value}
+            onChange={(e) => setMessage(e)}
+          ></textarea>
+          <button id={"submit"} onClick={() => handlePost()}>
+            Submit
+          </button>
         </div>
       ) : (
-        <div>
-          <input id="mail" type={"text"} onChange={handleChange}></input>
-          <br />
-          <button onClick={() => signInWithPassword(text, "")}>
-            サインイン
-          </button>
+        <div className={styles.main}>
+          <div className={styles.loginmain}>
+            <div className={styles.title}>
+              <h1>LANSHAREWARE</h1>
+            </div>
+            <div style={{width:"100%",display:"flex"}}>
+              <div className={styles.loginitem}>
+                <label>
+                  メールアドレス：
+                  <input
+                    id="mail"
+                    type={"text"}
+                    onChange={handleChange}
+                    className={styles.loginform}
+                  ></input>
+                </label>
+              </div>
+              <div className={styles.loginbutton}>
+                <button onClick={() => signInWithPassword(text, "")}>
+                  サインイン
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </>
